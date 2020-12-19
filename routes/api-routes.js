b1a7d60cdbf8631dc23ed96b5b536bdcca836fdd
bjsmak <brian.j.smak@gmail.com>
2020-12-19 17:13:20 -0500

    routes added

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,13 +1,3 @@
-// *********************************************************************************
-// api-routes.js - this file offers a set of routes for displaying and saving data to the db
-// *********************************************************************************
-
-// Dependencies
-// =============================================================
-
-const {
-    func
-} = require("Joi");
 var db = require("../models");
 
 //Routes
@@ -16,20 +6,19 @@ module.exports = function (app) {
 
 // GET route for getting all of the memes
 
-app.get("/api/memes/", function (req, res) {
-    db.Post.findAll({})
-        .then(function (memes_db) {
-            res.json(memes_db);
+app.get("/api/memes/:id", function (req, res) {
+    db.Meme.findOne({
+        where: {
+          id: req.params.id
+        }
+      })
+        .then(function(memes_db) {
+          res.json(memes_db);
         });
-});
+    });
 
-// Get route for returning memes 
-app.get("/api/memes/images/", function (req, res) {
-    db.Post.findAll({
-            where: {
-                image: req.params.image
-            }
-        })
+app.get("/api/memes", function (req, res) {
+    db.Meme.findAll({})
         .then(function (memes_db) {
             res.json(memes_db);
         });
@@ -37,15 +26,10 @@ app.get("/api/memes/images/", function (req, res) {
 
 
 // POST route for saving a new post
-app.post("/api/posts", function(req, res) {
+app.post("/api/memes", function(req, res) {
 console.log(req.body);
-    db.Post.create({
-        text: req.body.title,
-        image: req.body.body,
-        });
-})
-    .then(function(memes_db) {
+    db.Meme.create(req.body).then(function(memes_db) {
     res.json(memes_db);
     });
+});
 }
-
--- server.js
+++ server.js
@@ -1,13 +1,10 @@
 const express = require("express");
 const app = express();
-const db = require("./models");
+
 
 const PORT = process.env.PORT || 3000;
-db.sequelize.sync().then(function(){
-    app.listen(PORT, function(){
-        console.log("Listening on port %s", PORT);
-    });
-});
+
+const db = require("./models");
 
 app.use(express.static("public"));
 
@@ -22,11 +19,11 @@ const exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// const routes = require("");
+require("./routes/api-routes")(app);
 
 
 // Syncing our sequelize models and then starting our express app
-db.sequelize.sync({ force: true }).then(function() {
+db.sequelize.sync({ force: false }).then(function() {
     app.listen(PORT, function() {
       console.log("App listening on PORT " + PORT);
     });
