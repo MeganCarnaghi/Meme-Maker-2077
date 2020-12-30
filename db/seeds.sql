6c0de8d60481daf450bf94bea6a123134e7c76d0
Isaac Frishman <ifrishman89@gmail.com>
2020-12-29 20:24:44 -0600

    add jawsDB and mysql server data

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,14 +1,14 @@
 USE memes_db; 
 
-INSERT INTO memes(image) VALUES ('queenmeme');
-INSERT INTO memes(image) VALUES ('bettywhite');
-INSERT INTO memes(image) VALUES ('bidenobama');
-INSERT INTO memes(image) VALUES ('womanyellingcat');
-INSERT INTO memes(image) VALUES ('oldmanatdesk_');
-INSERT INTO memes(image) VALUES ('catroomguardian');
-INSERT INTO memes(image) VALUES ('everywhere');
-INSERT INTO memes(image) VALUES ('scaredcat');
-INSERT INTO memes(image) VALUES ('maury');
-INSERT INTO memes(image) VALUES ('willywonka');
-INSERT INTO memes(image) VALUES ('dude_and_girlfriend');
-INSERT INTO memes(image) VALUES ('drake');
+INSERT INTO Memes(image) VALUES ('queenmeme');
+INSERT INTO Memes(image) VALUES ('bettywhite');
+INSERT INTO Memes(image) VALUES ('bidenobama');
+INSERT INTO Memes(image) VALUES ('womanyellingcat');
+INSERT INTO Memes(image) VALUES ('oldmanatdesk_');
+INSERT INTO Memes(image) VALUES ('catroomguardian');
+INSERT INTO Memes(image) VALUES ('everywhere');
+INSERT INTO Memes(image) VALUES ('scaredcat');
+INSERT INTO Memes(image) VALUES ('maury');
+INSERT INTO Memes(image) VALUES ('willywonka');
+INSERT INTO Memes(image) VALUES ('dude_and_girlfriend');
+INSERT INTO Memes(image) VALUES ('drake');
